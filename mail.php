8f54042f4d60850a6a76574f28583dc1bc054d6b
OrosJ <joseoros9@gmail.com>
2023-11-13 00:47:14 -0400

    update

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
+        $mail_to = "jose.oros@uab.edu.bo";
         
         # Sender Data
         // $subject = trim($_POST["subject"]);
@@ -14,34 +14,34 @@
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Complete el formulario e intente otra vez.";
             exit;
         }
         
         # Mail Content
-        $content = "Name: $name\n";
+        $content = "Nombre: $name\n";
         $content .= "Email: $email\n\n";
-        $content .= "Message:\n$message\n";
+        $content .= "Mensaje:\n$message\n";
 
         # email headers.
-        $headers = "From: $name <$email>";
+        $headers = "De: $name <$email>";
 
         # Send the email.
         $success = mail($mail_to,$content, $headers);
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Gracias! se ha enviado el mensaje.";
         } else {
             # Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
+            echo "Oops! algo salió mal.";
         }
 
     } else {
         # Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Problema con la peticion.";
     }
 
 ?>
